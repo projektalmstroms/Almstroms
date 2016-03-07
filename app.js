9cb940d7448340368ed627141ac0e6a8dc151717
Tomas Johansson <tomtomjohansson@yahoo.se>
2016-03-07 10:46:57 +0100

    Added bookingsystem, resolved conflict

--- app.js
+++ app.js
@@ -11,13 +11,8 @@ var list = require('./routes/list');
 var booking = require('./routes/booking');
 var confirmation = require('./routes/confirmation');
 var newuser = require('./routes/newuser');
-<<<<<<< HEAD
 var adduser = require('./routes/adduser');
 var newCar = require('./routes/newcar');
-=======
-var addUser = require('./routes/addUser');
-
->>>>>>> booking
 
 var app = express();
 app.locals.appUser = "";
@@ -38,13 +33,8 @@ app.use('/booking', booking);
 app.use('/list', list);
 app.use('/confirmation', confirmation);
 app.use('/newuser', newuser);
-<<<<<<< HEAD
 app.use('/adduser', adduser);
 app.use('/newcar', newCar);
-
-=======
-app.use('/addUser', addUser);
->>>>>>> booking
 
 app.post('/login', function (req, res) {
    readFile.readJson(userData, loginUser);
@@ -64,7 +54,6 @@ app.post('/login', function (req, res) {
    }
 });
 
-<<<<<<< HEAD
 // check registration number
 app.post('/newcar', function (req, res, next) {
 
@@ -96,7 +85,8 @@ app.post('/newcar', function (req, res) {
     model: req.body.model,
   }
   console.log(newCar);
-=======
+});
+
 app.post('/logout',function(req,res){
    app.locals.appUser = "";
    res.writeHead(302,{'Location':'/'});
@@ -111,16 +101,8 @@ app.get('/*',function(req,res,next){
       res.writeHead(302,{'Location':'/'});
       res.end();
    }
->>>>>>> booking
 });
 
-
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> booking
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
